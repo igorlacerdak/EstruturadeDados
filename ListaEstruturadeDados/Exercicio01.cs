cd57ba0cdc045dd44100e9af3fc05c67c7d14410
Igor Lacerda <igorlacerdasantos@hotmail.com>
2019-10-28 19:29:00 -0300

    Inicio de Projeto

---
+++ ListaEstruturadeDados/Exercicio01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEstruturadeDados
+{
+    public class Exercicio01
+    {
+
+        public static void exercicio1()
+        {
+            Console.WriteLine("EXERCICIO 01 #\r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Digite os valores desejados separados por VIRGULA, e pressione enter: ");
+
+            var valoresDigitados = Console.ReadLine();
+            var vlVetor = valoresDigitados.Split(',');
+            valoresDigitados = String.Join(",", vlVetor);
+
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("Digite o valor para pesquisar no vetor: ");
+            var pesquisaValor = Console.ReadLine();
+
+            var resultado = false;
+
+            for (int i = 0; i < vlVetor.Length; i++)
+            {
+                if (pesquisaValor == vlVetor[i])
+                {
+                    Console.WriteLine("Sim, ele existe na posição: " + (i + 1));
+                    resultado = true;
+                }
+            }
+
+            if (resultado == false)
+            {
+                Console.WriteLine("Não, ele não existe");
+            }
+        }
+    }
+}
---
+++ ListaEstruturadeDados/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEstruturadeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Escolha o Exercicio Desejado:");
+            Console.WriteLine("\tExercicio - 1");
+            Console.WriteLine("\tExercicio - 2");
+            Console.WriteLine("\tExercicio - 3");
+            Console.WriteLine("\tExercicio - 4");
+            Console.WriteLine("\tExercicio - 5");
+            Console.WriteLine("\tExercicio - 6");
+            Console.WriteLine("\tExercicio - 7");
+            Console.Write("Qual exercicio? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Exercicio01.exercicio1(); 
+                    break;
+                case "2":
+                    Exercicio02.exercicio2();
+                    break;
+                case "3":
+                    Exercicio03.exercicio3();
+                    break;
+                case "4":
+                    Console.WriteLine($"Your result");
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+
+        }
+    }
+}
